401e0927bbaac33a7384b6702f838676236f6ed2
coolboy <30379973+joinName177@users.noreply.github.com>
2026-08-19 21:02:50 +0800

    Capture test-model concurrent ticket intake repair

--- internal/service/id_allocator.go
+++ internal/service/id_allocator.go
@@ -2,13 +2,17 @@ package service
 
 import (
 	"fmt"
+	"sync/atomic"
 	"time"
 )
 
-type ticketIDAllocator struct{}
+type ticketIDAllocator struct {
+	counter atomic.Int64
+}
 
 func newTicketIDAllocator() *ticketIDAllocator { return &ticketIDAllocator{} }
 
 func (a *ticketIDAllocator) Next(now time.Time) string {
-	return fmt.Sprintf("ticket-%d-%03d", now.UnixNano(), 1)
+	seq := a.counter.Add(1)
+	return fmt.Sprintf("ticket-%d-%03d", now.UnixNano(), seq)
 }
